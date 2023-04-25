9b36a455caf84b7fba3678b1edf90f3ce3184499
Wojciech Przyluski <przyluskiwojciech@gmail.com>
2023-04-25 18:22:24 +0200

    Implementacja losowego ulozenia kart, likwidacja redundancji kodu, dodanie przycisku zagraj ponownie

--- memory.js
+++ memory.js
@@ -1,38 +1,32 @@
+//zmiana opakowania kart z kazda w osobnym pojemniku na wszystkie w jednym. zmniejszyc portrety i zwiekszyc ich ilosc.
+
 var cards = ["ciri.png", "geralt.png", "jaskier.png", "jaskier.png", "iorweth.png", "triss.png", "geralt.png", "yen.png", "ciri.png", "triss.png", "yen.png", "iorweth.png"];
 
-//alert(cards[4]);
+// Funkcja mieszająca elementy tablicy w losowej kolejności
+function shuffle(array)
+{
+    for (let i = array.length - 1; i > 0; i--)
+    {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
 
-//console.log(cards);
+var shuffledCards = shuffle(cards);
 
-var c0 = document.getElementById('c0');
-var c1 = document.getElementById('c1');
-var c2 = document.getElementById('c2');
-var c3 = document.getElementById('c3');
 
-var c4 = document.getElementById('c4');
-var c5 = document.getElementById('c5');
-var c6 = document.getElementById('c6');
-var c7 = document.getElementById('c7');
+var loadedCards = [];
 
-var c8 = document.getElementById('c8');
-var c9 = document.getElementById('c9');
-var c10 = document.getElementById('c10');
-var c11 = document.getElementById('c11');
+for (var i = 0; i < 12; i++)
+{
+    loadedCards['c' + i] = document.getElementById('c' + i);
+    loadedCards['c' + i].addEventListener("click", (function (i)
+    {
+        return function () { revealCard(i); }
+    })(i));
+}
 
-c0.addEventListener("click", function () { revealCard(0); });
-c1.addEventListener("click", function () { revealCard(1); });
-c2.addEventListener("click", function () { revealCard(2); });
-c3.addEventListener("click", function () { revealCard(3); });
-
-c4.addEventListener("click", function () { revealCard(4); });
-c5.addEventListener("click", function () { revealCard(5); });
-c6.addEventListener("click", function () { revealCard(6); });
-c7.addEventListener("click", function () { revealCard(7); });
-
-c8.addEventListener("click", function () { revealCard(8); });
-c9.addEventListener("click", function () { revealCard(9); });
-c10.addEventListener("click", function () { revealCard(10); });
-c11.addEventListener("click", function () { revealCard(11); });
 
 var oneVisible = false;
 var turnCounter = 0;
@@ -50,6 +44,7 @@ function revealCard(nr)
     if (opacityValue != 0 && lock == false)
     {
         lock = true;
+
 
         var obraz = "url(img/" + cards[nr] + ")";
 
@@ -102,6 +97,11 @@ function hide2Cards(nr1, nr2)
     if (pairsLeft == 0)
     {
         $('.board').html('<h1>You win!<br>Done in ' + turnCounter + ' turns</h1>');
+        $('.board').append('<button id="play-again-btn">Zagraj ponownie</button>');
+        $('#play-again-btn').on('click', function ()
+        {
+            window.location.reload();
+        });
     }
 
     lock = false;
